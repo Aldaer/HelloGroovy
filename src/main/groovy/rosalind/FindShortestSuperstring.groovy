c5d868d39a0d669c62f5aa677630c198b63da442
Artem Lodygin <aldaer@yandex.ru>
2017-06-01 00:46:54 +0300

    Shortest Superstring - functioning way only

--- src/main/groovy/rosalind/FindShortestSuperstring.groovy
+++ src/main/groovy/rosalind/FindShortestSuperstring.groovy
@@ -1,50 +1,70 @@
 package rosalind
 
-import groovy.transform.Field
+class Solver {
+    private List<String> substrings
+    private int N
+    private int[][] extraLength
+    private int[] bestNextMatch
+    private int[] bestBefore
 
-@Field List<String> substrings = []
+    private List<Integer> sequence
 
-static <T> List<T> forEachSetBit(long x, Closure<T> action) {
-    long dx = x
-    def result = []
-    int i = 0
-    while (dx > 0) {
-        if ((dx & 1) > 0) result << action.call(i)
-        dx >>= 1
-        i++
+    // How long is the piece of s2 AFTER the prefix which is equal to s1's suffix
+    static int findExtraLength(String s1, String s2) {
+        def minL = Math.min(s1.length(), s2.length())
+        for (int x = minL; x > 0; x--)
+            if (s1[-x..-1] == s2[0..x - 1]) return s2.length() - x
+        s2.length()
     }
-    result
-}
 
-Closure<List<Tuple2<Long, Integer>>> withoutOneBit = { long x ->
-    forEachSetBit x, { i -> new Tuple2(x & ~(1 << i), i) }
-}
+    Solver(List<FASTA> sequences) {
+        N = sequences.size()
+        substrings = sequences.collect { it.seq }
 
-static String minimumCombo2(String s1, String s2) {
-    if (s2.contains(s1)) return s2
+        extraLength = new int[N][N]
+        bestNextMatch = new int[N]
+        bestBefore = new int[N]
+        Arrays.fill(bestNextMatch, -1)
+        Arrays.fill(bestBefore, -1)
 
-    for (int i = s2.length(); i > 0; i--)
-        if (s1.endsWith(s2.substring(0, i))) return s1 + s2.substring(i)
-
-    s1 + s2
-}
-
-superstring = { long multiIndex ->
-    def subindices = withoutOneBit.call(multiIndex)
-    subindices.collectMany { subindex ->
-        subindex.first == 0L ? [substrings[subindex.second]] :
-                superstring.call(subindex.first).collect { minimumCombo2(it, substrings[subindex.second]) }
+        (0..N - 1).each { int i ->
+            (0..N - 1).findAll { it != i }.each { int j ->
+                int el = findExtraLength(substrings[i], substrings[j])
+                extraLength[i][j] = el
+                if (el < substrings[j].length() / 2) {
+                    bestNextMatch[i] = j
+                    bestBefore[j] = i
+                }
+            }
+        }
     }
-}.memoize()
 
+    String assemblePieces() {
+        def result = new StringBuilder(substrings[sequence[0]])
+        (1..N - 1).each { i ->
+            String nextPiece = substrings[sequence[i]]
+            int nextOffset = extraLength[sequence[i - 1]][sequence[i]]
+            result << nextPiece[-nextOffset..-1]
+        }
+        result.toString()
+    }
+
+    String solve() {
+        int firstOne = bestBefore.findIndexOf { it == -1 }
+        sequence = (0..N - 1).collect {
+            def current = firstOne
+            firstOne = bestNextMatch[current]
+            return current
+        }
+        assemblePieces()
+    }
+}
 
 def sequences = FASTA.readFromConsole()
-def N = sequences.size()
-assert N < 63
-
-int allStrings = (1 << N) - 1
-substrings = sequences.collect { it.seq }
 
 def time = System.currentTimeMillis()
-println superstring.call(allStrings).sort { s1, s2 -> s1.length() - s2.length() }.get(0)
-println "Calculation time: ${System.currentTimeMillis() - time} ms"
+def solver = new Solver(sequences)
+def sequence = solver.solve()
+println "Processing time: ${System.currentTimeMillis() - time} ms"
+println "Resulting length: ${sequence.length()}"
+println sequence
