ae34e62e8f40674b4984525ef78cb69dfbcfd391
Artem Lodygin <artem_lodygin@epam.com>
2017-08-04 13:38:49 +0300

    Dynamic method calling in Groovy

---
+++ src/test/groovy/DynamicMethodCall.groovy
@@ -0,0 +1,17 @@
+/**
+ * Created by Artem_Lodygin on 04-Aug-17.
+ */
+class DynamicMethodCallTarget {
+    void one(String param) {
+        println "1-$param-1"
+    }
+    void two(String param) {
+        println "2-$param-2"
+    }
+}
+
+def dmc = new DynamicMethodCallTarget()
+def methodName = 'one'
+dmc."$methodName"("uno")
+methodName = 'two'
+dmc."$methodName"("duo")
